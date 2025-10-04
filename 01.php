75ca6ee6bf293b0357943afb917d102c948d0739
07-kaya <aqua4leaf+pc@gmail.com>
2025-10-05 00:56:57 +0900

    DOCTYPE

--- 01.php
+++ 01.php
@@ -1,4 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>サイトタイトル</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <footer>
+    <?php include("header.html"); ?>
+  </footer>
 
 
 
@@ -10,3 +21,13 @@ $prev = 1;
 $next = 3;
 include("gallery-template.php");
 ?>
+
+
+
+
+
+  <footer>
+    <?php include("footer.html"); ?>
+  </footer>
+</body>
+</html>
